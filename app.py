ca6cd97a18151cc5d5fc1b419cb742dd8b6a6b91
Arthemyst <pawel.lukmin@gmail.com>
2022-08-23 21:59:49 +0200

    Feat: Added condition to prevent wrong url.

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ import scraping_functions as sf
 nofluffjobs_list = sf.nofluffjobs_page_job_offers()
 df_raw_1 = pd.DataFrame.from_records(nofluffjobs_list)
 df_1 = df_raw_1.copy()
-df_1["publication_date"] = pd.to_datetime(
-    df_1["publication_date"], infer_datetime_format=True
-)
-df_1.drop_duplicates(
-    subset=["publication_date", "company", "title"], inplace=True, ignore_index=True
-)
+if len(df_1) != 0:
+
+    df_1["publication_date"] = pd.to_datetime(
+        df_1["publication_date"], infer_datetime_format=True
+    )
+    df_1.drop_duplicates(
+        subset=["publication_date", "company", "title"], inplace=True, ignore_index=True
+    )
 
 
 bulldogjob_list = sf.bulldog_page_job_offers()
@@ -26,8 +28,9 @@ df_2.drop_duplicates(
 
 
 df_3 = pd.concat([df_1, df_2], axis=0, ignore_index=True)
-df_3.sort_values(["publication_date"], inplace=True, ascending=False)
 df_3["publication_date"] = df_3["publication_date"].dt.strftime("%Y-%m-%d")
+df_3.sort_values(["publication_date"], inplace=True, ascending=False)
+# df_3.reset_index(inplace=True)
 
 sector = df_3.groupby("position")
 
@@ -45,6 +48,6 @@ selected_title = st.sidebar.multiselect(
 df_selected = df_3[
     (df_3["position"].isin(selected_position)) & (df_3["title"].isin(selected_title))
 ]
-st.title("Job offers:")
 
+st.title("Job offers:")
 st.dataframe(df_selected)
