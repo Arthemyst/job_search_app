83f84ba7c98cfd651ce0c864556f9b1abe1a6f4d
Arthemyst <pawel.lukmin@gmail.com>
2022-08-23 21:03:11 +0200

    Feat: Created app.py to visualize job offers details in website made with streamlit.

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 import scraping_functions as sf
 import pandas as pd
+import streamlit as st
+
 
 nofluffjobs_list = sf.nofluffjobs_page_job_offers()
 df_raw_1 = pd.DataFrame.from_records(nofluffjobs_list)
 df_1 = df_raw_1.copy()
 df_1['publication_date'] = pd.to_datetime(df_1['publication_date'], infer_datetime_format=True)
 df_1.drop_duplicates(subset=['publication_date', 'company', 'title'], inplace=True, ignore_index=True)
-print(df_1)
-print('*'*30)
+
 
 bulldogjob_list = sf.bulldog_page_job_offers()
 df_raw_2 = pd.DataFrame.from_records(bulldogjob_list)
 df_2 = df_raw_2.copy()
 df_2['publication_date'] = pd.to_datetime(df_2['publication_date'], infer_datetime_format=True)
 df_2.drop_duplicates(subset=['publication_date', 'company', 'title'], inplace=True, ignore_index=True)
-print(df_2)
-print('*'*30)
 
-print('After merging:')
+
 df_3 = pd.concat([df_1, df_2], axis=0, ignore_index=True)
 df_3.sort_values(['publication_date'], inplace=True, ascending=False)
-print(df_3)
+df_3["publication_date"] = df_3["publication_date"].dt.strftime("%Y-%m-%d")
+
+sector = df_3.groupby('position')
+
+# Sidebar - Position selection
+sorted_position_unique = sorted( df_3['position'].unique() )
+selected_position = st.sidebar.multiselect('Position', sorted_position_unique, sorted_position_unique)
+# Sidebar - Title selection
+sorted_title_unique = sorted( df_3['title'].unique() )
+selected_title = st.sidebar.multiselect('Title', sorted_title_unique, sorted_title_unique)
+# Filtering data
+df_selected = df_3[ (df_3['position'].isin(selected_position)) & (df_3['title'].isin(selected_title)) ]
+st.title('Job offers:')
+
+st.dataframe(df_selected)
