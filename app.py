b025f843dc2026d6eb4e39379bdc075a1a54754d
Arthemyst <pawel.lukmin@gmail.com>
2022-08-24 17:04:30 +0200

    Feat: Added feature with date range slider to choose range of offers publication dates.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import pandas as pd
+import datetime
+
 import streamlit as st
 
 import scraping_functions as sf
 
+st.title("Job offers:")
 df = sf.merge_dataframes()
 
 
@@ -18,10 +20,26 @@ sorted_title_unique = sorted(df["title"].unique())
 selected_title = st.sidebar.multiselect(
     "Title", sorted_title_unique, sorted_title_unique
 )
-# Filtering data
-df_selected = df[
-    (df["position"].isin(selected_position)) & (df["title"].isin(selected_title))
-]
+# Sidebar - date range
 
-st.title("Job offers:")
-st.dataframe(df_selected)
+last_day = df["publication_date"].min()
+last_date = datetime.datetime.strptime(last_day, "%Y-%m-%d")
+today = datetime.date.today()
+
+end_date = st.sidebar.date_input("End date", today)
+start_date = st.sidebar.date_input("Start date", last_date)
+
+
+# Filtering data
+if start_date < end_date:
+    df_selected = df[
+        (df["position"].isin(selected_position))
+        & (
+            df["title"].isin(selected_title)
+            & (df["publication_date"] >= str(start_date))
+            & (df["publication_date"] <= str(end_date))
+        )
+    ]
+    st.dataframe(df_selected)
+else:
+    st.write("Error: End date must fall after start date.")
