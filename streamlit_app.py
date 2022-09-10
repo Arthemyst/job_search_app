02a9100e8292c95e07f57f5248822ad17902bc29
Pawel <pawel.lukmin@gmail.com>
2022-09-10 13:34:19 +0200

    Change: Used black and isort.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 import datetime
 from turtle import color
 
+import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
+from plotly.subplots import make_subplots
 
 import scraping_functions as sf
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-
 
 st.title("Job offers:")
 df = sf.merge_dataframes()
@@ -46,25 +45,42 @@ if start_date <= end_date:
     ]
     if len(df_selected) != 0:
         st.dataframe(df_selected)
-        fig = make_subplots(rows=1, cols=2, subplot_titles=("Count of offers by website.", "Count of offers by position."))
-        df_selected_count_by_website = df_selected.groupby('website').count()
-        df_selected_count_by_position = df_selected.groupby('position').count()
-        fig.add_trace(go.Bar(x=df_selected_count_by_website.index, 
-                            y=df_selected_count_by_website['company'],name='website', showlegend=False),
-                            row=1, col=1)
-        fig.add_trace(go.Bar(x=df_selected_count_by_position.index, 
-                            y=df_selected_count_by_position['company'],name='position', showlegend=False), 
-                            row=1, col=2)
+        fig = make_subplots(
+            rows=1,
+            cols=2,
+            subplot_titles=(
+                "Count of offers by website.",
+                "Count of offers by position.",
+            ),
+        )
+        df_selected_count_by_website = df_selected.groupby("website").count()
+        df_selected_count_by_position = df_selected.groupby("position").count()
+        fig.add_trace(
+            go.Bar(
+                x=df_selected_count_by_website.index,
+                y=df_selected_count_by_website["company"],
+                name="website",
+                showlegend=False,
+            ),
+            row=1,
+            col=1,
+        )
+        fig.add_trace(
+            go.Bar(
+                x=df_selected_count_by_position.index,
+                y=df_selected_count_by_position["company"],
+                name="position",
+                showlegend=False,
+            ),
+            row=1,
+            col=2,
+        )
         # Update yaxis properties
         fig.update_yaxes(title_text="Count", row=1, col=1)
         fig.update_yaxes(title_text="Count", row=1, col=2)
-        fig.update_layout( 
-            legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=1.02,
-            xanchor="right",
-            x=1))
+        fig.update_layout(
+            legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
+        )
         st.plotly_chart(fig, use_container_width=True)
 
     else:
